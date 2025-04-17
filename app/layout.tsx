105a76306ed15441d7ffa30beba2a520a0eb705c
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2025-04-17 16:30:05 -0400

    Nextra init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import {Footer, Layout, Navbar} from 'nextra-theme-docs'
-import {Banner, Head} from 'nextra/components'
-import {getPageMap} from 'nextra/page-map'
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import Link from "next/link";
 
@@ -12,40 +12,41 @@ export const metadata = {
 const banner = <Banner storageKey="some-key">This template was created with 🩸 and 💦 by <Link href="https://github.com/phucbm">PHUCBM</Link> 🐧</Banner>
 const navbar = (
     <Navbar
-        logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20}/>}
-        // ... Your additional navbar options
+        logo={<img src="/images/general/logo.webp" alt="Logo" width={100} height={20} />}
+    // ... Your additional navbar options
     />
 )
 const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
 
-export default async function RootLayout({children}) {
+export default async function RootLayout({ children }) {
     return (
         <html
             // Not required, but good for SEO
             lang="en"
             // Required to be set
             dir="ltr"
-            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-            suppressHydrationWarning
+        // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
         >
-        <Head
+            <Head
             // ... Your additional head options
-        >
-            <link rel="shortcut icon" href="/images/general/icon.svg"/>
-            {/* Your additional tags should be passed as `children` of `<Head>` element */}
-        </Head>
-        <body>
-        <Layout
-            banner={banner}
-            navbar={navbar}
-            pageMap={await getPageMap()}
-            docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
-            footer={footer}
-            // ... Your additional layout options
-        >
-            {children}
-        </Layout>
-        </body>
+            >
+                <link
+                    rel="shortcut icon" href="/images/general/icon.webp" />
+                {/* Your additional tags should be passed as `children` of `<Head>` element */}
+            </Head>
+            <body
+            >
+                <Layout
+                    // banner={banner}
+                    navbar={navbar}
+                    pageMap={await getPageMap()}
+                    docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
+                    footer={footer}
+                // ... Your additional layout options
+                >
+                    {children}
+                </Layout>
+            </body>
         </html>
     )
 }
